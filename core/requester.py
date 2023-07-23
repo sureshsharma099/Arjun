eb99b74e19906f46b7eff1be016abda7d399a13b
Suresh Sharma <122967826+sureshsharma099@users.noreply.github.com>
2023-07-23 17:23:33 +0530

    Update requester.py

--- core/requester.py
+++ core/requester.py
@@ -17,9 +17,9 @@ def requester(url, data, headers, GET, delay):
     time.sleep(delay)
     headers['Host'] = re.search(r'https?://([^/]+)', url).group(1)
     if GET:
-        response = requests.get(url, params=data, headers=headers, verify=False, allow_redirects=False, timeout=10)
+        response = requests.get(url, params=data, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}, verify=False, allow_redirects=False, timeout=10)
     elif core.config.globalVariables['jsonData']:
-        response = requests.post(url, json=data, headers=headers, verify=False, allow_redirects=False, timeout=10)
+        response = requests.post(url, json=data, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}, verify=False, allow_redirects=False, timeout=10)
     else:
-        response = requests.post(url, data=data, headers=headers, verify=False, allow_redirects=False, timeout=10)
+        response = requests.post(url, data=data, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}, verify=False, allow_redirects=False, timeout=10)
     return response
